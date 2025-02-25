2896c5362c6eb901b1256135ea48ca0c4e68337b
Jatan-Mathasoliya <jatan.codinggita@gmail.com>
2025-02-25 15:24:23 +0530

    frontend code

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Toaster } from './components/ui/sonner.jsx'
-import { SocketProvider } from './context/socketContext.jsx'
+import { SocketProvider } from './context/SocketContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
